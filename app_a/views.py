443d1299f21e4f92d8e3ed12bb15b043ad66bf5e
bre <iwanttosignup321@gmail.com>
2022-07-14 21:44:58 +0800

    7.14

--- app_a/views.py
+++ app_a/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Topic
-from .forms import TopicForm 
+from .forms import TopicForm, EntryForm 
 # Create your views here.
 def index(request):
     return render(request,'app_a/index.html')
@@ -19,7 +19,7 @@ def topic(request,topic_id):
     context={'topics':topics,'entries':entries}
     return render(request,'app_a/topic.html',context)
 
-def new_topic(request):
+#def new_topic(request):
     if request.method != 'POST':
         form=TopicForm()
     else:
@@ -30,3 +30,33 @@ def new_topic(request):
             return redirect('app_a:topics')
     context={'form':form}        
     return render(request,'app_a/new_topic.html',context)
+def new_topic(request):
+    if request.method=='POST':
+        form=TopicForm(data=request.POST)
+        if form.iv_valid():
+            form.save()
+            return redirect('app_a:topics')
+    else:
+        form=TopicForm()
+        context={'form':form}
+        return render(request,'app_a/new_topic.html',context)
+
+
+
+
+
+
+
+def new_entry(request,topic_id):
+    topic=Topic.objects.get(id=topic_id)
+    if request.method !='POST':
+        form = EntryForm()
+    else:
+        form=EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry=form.save(commit=False)  
+            new_entry.topic=topic 
+            new_entry.save()
+            return redirect('app_a:topic',topic_id=topic_id)
+    context={'topic':topic,'form':form}
+    return render(request, 'app_a/new_entry.html',context)
